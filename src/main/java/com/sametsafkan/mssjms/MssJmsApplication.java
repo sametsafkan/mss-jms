8380a87eabbd9ac75ab7e509a59dfe28c2e38963
Samet Safkan <sametsafkan@gmail.com>
2020-04-23 13:51:39 +0300

    initial commit.

---
+++ src/main/java/com/sametsafkan/mssjms/MssJmsApplication.java
@@ -0,0 +1,13 @@
+package com.sametsafkan.mssjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MssJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MssJmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sametsafkan/mssjms/MssJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sametsafkan.mssjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MssJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
